4b6aa5103c2ed3751dda6265085dc48427c1b46b
ELDementor <115427403+ELDementor@users.noreply.github.com>
2022-12-19 17:48:32 +0300

    Create task_2.py

---
+++ practice_4/task_2.py
@@ -0,0 +1,15 @@
+import git
+with open("git.txt") as source:
+    git_url = source.readlines()
+result={}
+git_url=[url.rstrip() for url in git_url]
+for url in git_url:
+    try:
+        git.Repo.clone_from(url, f"{url[url.rfind('/')+1:]}-https")
+        result[url] = 'OK'
+    except:
+        result[url]='FAIL'
+with open("result.txt", "w") as resultFile:
+    for i in git_url:
+        resultFile.write(f"{i}\t")
+        resultFile.write(f"{result[i]}\n")
