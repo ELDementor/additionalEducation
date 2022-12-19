db319a5e950d70180b0574b8e5ed763a0bfdc753
ELDementor <115427403+ELDementor@users.noreply.github.com>
2022-12-19 17:50:31 +0300

    Update and rename practice4/task1.py to practice_4/task1.py

--- practice4/task1.py
+++
@@ -1,7 +0,0 @@
-def numberGenerator(start,end): 
-     for i in range(start,end+2,2): 
-         yield i 
- if __name__ == '__main__': 
-     value=numberGenerator(2,100) 
-     for i in value: 
-         print(i)
